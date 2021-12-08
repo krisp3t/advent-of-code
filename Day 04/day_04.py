c81634fc85c33fb7cceeb78068aac25d4228d55b
krisp3t <77499501+krisp3t@users.noreply.github.com>
2021-12-08 11:24:15 +0100

    Add Day 4 puzzle 2 solutions

--- Day 04/day_04.py
+++ Day 04/day_04.py
@@ -33,7 +33,34 @@ def day_04_puzzle_1(draw, tables):
 
 
 def day_04_puzzle_2(vals):
-    return
+    marks = np.zeros_like(tables)
+    win_order = []
+    last_num = None
+
+    def calc_unmarked(table_ix):
+        it = np.nditer(marks[table_ix], flags=["multi_index"])
+        return np.sum(
+            [(tables[(table_ix, *it.multi_index)]) for mark in it if mark == 0]
+        )
+
+    for called_num in draw:
+        # Iterate over every element in tables
+        it = np.nditer(tables, flags=["multi_index"])
+        for tables_num in it:
+            table_ix, x, y = it.multi_index
+            if table_ix in win_order:
+                continue
+
+            # Place mark
+            if called_num == tables_num:
+                marks[(it.multi_index)] = 1
+
+                # Check win condition
+                if np.all(marks[table_ix, x, :]) or np.all(marks[table_ix, :, y]):
+                    win_order.append(table_ix)
+                    last_num = called_num
+
+    return calc_unmarked(win_order[-1]) * last_num
 
 
 print(f"Solution for Day 4 Puzzle 1: {day_04_puzzle_1(draw, tables)}")
