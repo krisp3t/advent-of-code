573b408a1ad0c090d55712a128a716c69b04da04
krisp3t <77499501+krisp3t@users.noreply.github.com>
2023-12-04 16:31:41 +0100

    Add Day 01 Part 2

--- 2023/src/bin/01.rs
+++ 2023/src/bin/01.rs
@@ -1,9 +1,6 @@
 advent_of_code::solution!(1);
 
-use regex::Regex;
-
 pub fn part_one(input: &str) -> Option<u32> {
-    let re = Regex::new(r"[0-9]").unwrap();
     let sum: u32 = input
         .trim()
         .lines()
@@ -17,7 +14,28 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let sum: u32 = input
+        .trim()
+        .lines()
+        .map(|line|
+            line.to_string()
+                .replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine")
+        )
+        .filter_map(|line| {
+            let first_digit = line.chars().find(|c| c.is_digit(10))?;
+            let last_digit = line.chars().rev().find(|c| c.is_digit(10))?;
+            format!("{}{}", first_digit, last_digit).parse::<u32>().ok()
+        })
+        .sum();
+    return Some(sum);
 }
 
 #[cfg(test)]
@@ -26,13 +44,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 1));
         assert_eq!(result, Some(142));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        assert_eq!(result, Some(281));
     }
 }
