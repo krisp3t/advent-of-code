d0893946f2460ccf1daa3dac68993cb7ebd73a8f
krisp3t <77499501+krisp3t@users.noreply.github.com>
2023-12-06 14:41:02 +0100

    Begin write parser

--- 2023/src/bin/02.rs
+++ 2023/src/bin/02.rs
@@ -1,41 +1,65 @@
 advent_of_code::solution!(2);
 
+use std::collections::HashMap;
+use nom::{
+    IResult,
+    sequence::preceded,
+    bytes::complete::tag,
+    character::complete::digit1,
+};
+
+#[derive(Debug, PartialEq, Eq, Hash)]
 enum CubeColor {
     Red,
     Green,
-    Blue
+    Blue,
 }
+
 struct Cubes {
     color: CubeColor,
-    number: u8
+    qty: usize,
 }
+
 struct Game {
-    id: u32,
-    pulls: Vec<Vec<Cubes>>
-}
-struct Games {
-    games: Vec<Game>
+    id: usize,
+    pulls: Vec<Vec<Cubes>>,
 }
 
-pub fn parse_games(input: &str) -> Option<Games> {
-    let mut games = Games { games: Vec::new() };
-    for line in input.lines() {
-        line.starts_with("Game").expect("Invalid input");
-        let id = line.split(" ").collect::<Vec<&str>>()[1].parse::<u32>().unwrap();
-        let mut pulls = Vec::new();
-        for pull in line.split(";") {
+impl Game {
+    // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    fn parse(input: &str) -> IResult<&str, Self> {
+        let (input, id) = preceded(
+            tag("Game "),
+            digit1,
+        )(input)?;
 
-        }
+        Ok((input, Self {
+            id: id.parse().unwrap(),
+            pulls: Vec::new(),
+        }))
     }
-    None
 }
+
 pub fn part_one(input: &str) -> Option<u32> {
+    let max_cubes: HashMap<CubeColor, usize> = HashMap::from([
+        (CubeColor::Red, 12),
+        (CubeColor::Green, 13),
+        (CubeColor::Blue, 14),
+    ]);
+
+    let possible_games: Vec<Game> = input
+        .trim()
+        .lines()
+        .map(|g| Game::parse(g).expect("Parsing input failed").1)
+        .collect();
+
     None
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     None
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -44,7 +68,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, 8);
+        assert_eq!(result, Some(8));
     }
 
     #[test]
